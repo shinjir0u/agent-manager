c319bad3f9562457db333ec919907c73857a9239
Shinjirou <htooagwai9@gmail.com>
2025-12-18 10:46:49 +0630

    Separate admin controller for easy access

---
+++ src/main/java/agentmanager/backoffice/rest/RegistrationController.java
@@ -0,0 +1,107 @@
+package agentmanager.backoffice.rest;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import agentmanager.registration.model.Registration;
+import agentmanager.registration.service.RegistrationService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/registration")
+public class RegistrationController {
+
+	private final Logger logger = LogManager.getLogger(RegistrationController.class);
+
+	@Autowired
+	private RegistrationService registrationService;
+
+	@GetMapping("/list")
+	public ResponseEntity<List<RegistrationResponse>> getRegistrations() {
+		List<Registration> registrations = registrationService.getRegistrations();
+		List<RegistrationResponse> response = registrations.stream()
+				.map(registration -> new RegistrationResponse(registration.getId(), registration.getAgentName(),
+						registration.getPhoneNumber(), registration.getRegisteredAt(),
+						registration.getSaleExecutive().getUsername()))
+				.collect(Collectors.toList());
+		logger.info("Fetched registrations: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@GetMapping("/details/{id}")
+	public ResponseEntity<RegistrationResponse> getRegistration(@PathVariable Long id) {
+		Registration registration = registrationService.getRegistration(id);
+		RegistrationResponse response = new RegistrationResponse(registration.getId(), registration.getAgentName(),
+				registration.getPhoneNumber(), registration.getRegisteredAt(),
+				registration.getSaleExecutive().getUsername());
+		logger.info("Fetched registration: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<RegistrationResponse> updateRegistration(@PathVariable Long id,
+			@RequestBody RegistrationUpdateRequest request) {
+		Registration registrationFetched = registrationService.getRegistration(id);
+		if (registrationFetched == null)
+			throw new IllegalArgumentException("No such registration with id: " + id);
+
+		Registration registration = registrationService.updateRegistration(id, request.getPhoneNumber());
+		RegistrationResponse response = new RegistrationResponse(registration.getId(), registration.getAgentName(),
+				registration.getPhoneNumber(), registration.getRegisteredAt(),
+				registration.getSaleExecutive().getUsername());
+		logger.info("Updated registration: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	static class RegistrationUpdateRequest {
+
+		@JsonProperty("phone_number")
+		private String phoneNumber;
+
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	static class RegistrationResponse {
+
+		private Long id;
+
+		@JsonProperty("agent_name")
+		private String agentName;
+
+		@JsonProperty("phone_number")
+		private String phoneNumber;
+
+		@JsonProperty("registered_at")
+		private Date registeredAt;
+
+		@JsonProperty("registered_by")
+		private String registeredBy;
+
+	}
+
+}
---
+++ src/main/java/agentmanager/backoffice/rest/SaleExecutiveController.java
@@ -0,0 +1,151 @@
+package agentmanager.backoffice.rest;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import agentmanager.saleexecutive.model.SaleExecutive;
+import agentmanager.saleexecutive.service.SaleExecutiveService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/sale_executive")
+public class SaleExecutiveController {
+
+	private final Logger logger = LogManager.getLogger(SaleExecutiveController.class);
+
+	@Autowired
+	private SaleExecutiveService saleExecutiveService;
+
+	@GetMapping("/list")
+	public ResponseEntity<List<SaleExecutiveResponse>> getSaleExecutives() {
+		List<SaleExecutive> saleExecutives = saleExecutiveService.getSaleExecutives();
+		List<SaleExecutiveResponse> response = saleExecutives
+				.stream().map(saleExecutive -> new SaleExecutiveResponse(saleExecutive.getId(),
+						saleExecutive.getUsername(), saleExecutive.getEmail(), saleExecutive.getPhoneNumber()))
+				.collect(Collectors.toList());
+		logger.info("Fetched sale executives: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@GetMapping("/details/{id}")
+	public ResponseEntity<SaleExecutiveResponse> getSaleExecutive(@PathVariable Long id) {
+		SaleExecutive saleExecutive = saleExecutiveService.getSaleExecutive(id);
+		SaleExecutiveResponse response = new SaleExecutiveResponse(saleExecutive.getId(), saleExecutive.getUsername(),
+				saleExecutive.getEmail(), saleExecutive.getPhoneNumber());
+		logger.info("Fetched sale executive: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<SaleExecutiveResponse> addSaleExecutive(@RequestBody SaleExecutiveCreateRequest request) {
+		SaleExecutive saleExecutive = saleExecutiveService.addSaleExecutive(request.getUsername(), request.getEmail(),
+				request.getPassword(), request.getPhoneNumber());
+		SaleExecutiveResponse response = new SaleExecutiveResponse(saleExecutive.getId(), saleExecutive.getUsername(),
+				saleExecutive.getEmail(), saleExecutive.getPhoneNumber());
+		URI saleExecutiveLocation = generateEntryUri(saleExecutive.getId());
+		logger.info("Sale Executive: {} created at: {}", response, saleExecutiveLocation);
+
+		return ResponseEntity.created(saleExecutiveLocation).contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<SaleExecutiveResponse> updateSaleExecutiveJson(@PathVariable Long id,
+			@RequestBody SaleExecutiveUpdateRequest request) {
+		SaleExecutive saleExecutiveFetched = saleExecutiveService.getSaleExecutive(id);
+		if (saleExecutiveFetched == null)
+			throw new IllegalArgumentException("No such sale executive with id: " + id);
+
+		SaleExecutive saleExecutive = saleExecutiveService.updateSaleExecutive(id, request.getEmail(),
+				request.getPhoneNumber());
+		SaleExecutiveResponse response = new SaleExecutiveResponse(saleExecutive.getId(), saleExecutive.getUsername(),
+				saleExecutive.getEmail(), saleExecutive.getPhoneNumber());
+
+		logger.info("Updated admin: {}", response);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteSaleExecutive(@PathVariable Long id) {
+		SaleExecutive saleExecutiveFetched = saleExecutiveService.getSaleExecutive(id);
+		if (saleExecutiveFetched == null)
+			throw new IllegalArgumentException("No such sale executive with id: " + id);
+
+		saleExecutiveService.deleteSaleExecutive(id);
+		logger.info("Sale executive with id {} is deleted", id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	private URI generateEntryUri(Object entryId) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(entryId).toUri();
+		return uri;
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	static class SaleExecutiveCreateRequest {
+
+		private String username;
+
+		private String email;
+
+		private String password;
+
+		@JsonProperty("phone_number")
+		private String phoneNumber;
+
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	static class SaleExecutiveUpdateRequest {
+
+		private String email;
+
+		@JsonProperty("phone_number")
+		private String phoneNumber;
+
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	static class SaleExecutiveResponse {
+
+		private Long id;
+
+		private String username;
+
+		private String email;
+
+		@JsonProperty("phone_number")
+		private String phoneNumber;
+
+	}
+
+}
