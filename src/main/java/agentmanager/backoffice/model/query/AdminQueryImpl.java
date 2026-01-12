61f9afe16e291578e3dd6cf3c5d6e22e935899c0
Shinjirou <htooagwai9@gmail.com>
2026-01-12 10:06:58 +0630

    Refactor ramining admin query with boolean builder

--- src/main/java/agentmanager/backoffice/model/query/AdminQueryImpl.java
+++ src/main/java/agentmanager/backoffice/model/query/AdminQueryImpl.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.BooleanBuilder;
 
 import agentmanager.backoffice.model.Admin;
 import agentmanager.backoffice.repository.AdminRepository;
@@ -22,19 +22,10 @@ public class AdminQueryImpl implements AdminQuery {
 	public Page<Admin> getAdmins(Integer page, Integer size, String sort, String direction, String username,
 			String email) {
 
-		BooleanExpression where = null;
-		if (username != null)
-			where = AdminFilter.withUsername(username);
+		BooleanBuilder builder = new BooleanBuilder();
+		builder.and(AdminFilter.withUsername(username)).and(AdminFilter.withEmail(email));
 
-		if (email != null) {
-			BooleanExpression emailExpression = AdminFilter.withEmail(email);
-			where = (where != null) ? where.and(emailExpression) : emailExpression;
-		}
-
-		if (where == null)
-			return adminRepository.findAll(PageRequest.of(page, size, Sort.Direction.fromString(direction), sort));
-
-		return adminRepository.findAll(where, PageRequest.of(page, size, Sort.Direction.fromString(direction), sort));
+		return adminRepository.findAll(builder, PageRequest.of(page, size, Sort.Direction.fromString(direction), sort));
 
 	}
 
